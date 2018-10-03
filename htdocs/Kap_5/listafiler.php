f8045af2da5b32fe00b418f5c5aee0104b823610
Vincent <vincent.nordeman@learnet.se>
2018-10-03 13:33:35 +0200

    bildgalleri i php med foreach

--- htdocs/Kap_5/listafiler.php
+++ htdocs/Kap_5/listafiler.php
@@ -4,23 +4,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Lista</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bildgalleri.css">
 </head>
-<body>
+<body> 
     <?php
 
-    $sokvag = "C:\Users\larsvincent.nordema\Pictures\Screenshots";
-
+    $sokvag = "./bilder";
     $filer = scandir($sokvag);
+
+    echo "<div class=\"container\">\n";
+    echo "<h1>Bild galleri</h1>\n";
 
     echo "<h3>Skriv ut alla filer som hittats</h3>";
     foreach ($filer as $fil) {
         if ($fil != "." && $fil != "..") {
-            
             echo "<p>$fil</p>";
+            echo "<div class=\"ros\">\n";
+            echo "<img class=\"ram vanster\" src=\"./bilder/sam-ekpil-522518-unsplash.jpg\" alt=\"Vinter  (Hundar)\">\n";
+            echo "<p><strong>Polarsken</strong> (Hundar?), Norrsken med hundar</p>";
+            echo "<hr>";
+        echo "</div>";
         }
     }
-
+    echo "</div>\n";
     ?>
 </body>
 </html>

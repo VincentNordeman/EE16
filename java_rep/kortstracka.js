0e40de92f47766f84e8a287611b68ac7696f0e9d
Vincent <vincent.nordeman@learnet.se>
2018-10-22 14:52:01 +0200

    Övat på query selector och addventaddlistener

---
+++ java_rep/kortstracka.js
@@ -0,0 +1,37 @@
+/* 
+ * Räknar ut en körsträckan för en bil.
+ * Mata in hastighet och tid.
+ * Få avstånd som svar.
+ */
+
+/* Kör koden när webbsidan laddats klart */
+window.onload = start;
+
+function start() {
+
+    /* Välj ut alla elementen att jobba med: target */
+    const elementHastiget = document.querySelector(".hastighet")
+    const elementTid = document.querySelector(".tid");
+    const elementRaknaUt = document.querySelectorAll(".raknaUt");
+    const elementSvar = document.querySelectorAll(".svar");
+    console.log(elementHastighet);
+    console.log(elementTid);
+    console.log(elementRaknaUt);
+    console.log(elementSvar);
+
+    /* Lyssna på knappen */
+    elementRaknaUt = addEventListener("click, raknaUt");
+
+    /* Räkna ut avståndet */
+    function raknaUt() {
+
+        /* Läs av hastigheten och tiden */
+        var hastighet = elementHastiget.textContent;
+        var tid = elementTid.value;
+
+        /* Räkna ut avståndet = hastigheten * tid */
+        var avstand = hastighet * tid / 60;
+
+        /* Skriv ut resultatet tex 'Körsträckan blir 57 km.' */
+    } elementSvar.textContent = "Körsträckan blir" + avstand + "km.";
+}

4a7c9358ca60a61027100f98cefb5edb41173d50
Vincent <vincent.nordeman@learnet.se>
2019-02-13 13:25:22 +0100

    maze

--- oop/labyrint/maze.js
+++ oop/labyrint/maze.js
@@ -13,6 +13,7 @@ function start() {
     const canvas = document.querySelector("#myCanvas");
     var ctx = canvas.getContext("2d");
 
+    var points = 0;
     var map = [];
     var key = [];
     var spelare = {
@@ -20,37 +21,94 @@ function start() {
         x: 0
     };
 
-    var monster = {
-        y: 0,
-        x: 0
-    };
-
     var imgSpelare = new Image();
     imgSpelare.src = "./../bilder/IMG_20170903_220047_205.png";
-    var imgMonster = new Image();
-    imgMonster.src = "./../bilder/Profile.jpg";
 
     map = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
-        [1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1],
-        [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
 
-    function reset() {
-        spelare.y = 1;
-        spelare.x = 2;
-
-        monster.y = 2;
-        monster.x = 4;
+    class Mynt {
+        constructor() {
+            this.x = 0;
+            this.y = 0;
+            this.imgMynt = new Image();
+            this.imgMynt.src = "./../bilder/download.jpg";
+            this.live = true;
+        }
+        reset() {
+            this.x = Math.ceil(Math.random() * 12);
+            this.y = Math.ceil(Math.random() * 9);
+        }
+        ritaMynt() {
+            ctx.beginPath();
+            ctx.drawImage(this.imgMynt, this.x * 60 + 5, this.y * 60 + 5, 55, 55);
+            ctx.closePath();
+        }
+        uppdateraMynt() {
+            if (this.live) {
+                if (map[this.y][this.x] == 1) {
+                    this.x = Math.ceil(Math.random() * 12);
+                    this.y = Math.ceil(Math.random() * 9);
+                } else {
+                    this.ritaMynt();
+                }
+            }
+        }
+        getPoint() {
+            if (this.live && this.x == spelare.x && this.y == spelare.y) {
+                points += 10;
+                this.live = false;
+            }
+        }
     }
 
+    var mynt1 = new Mynt();
+    var mynt2 = new Mynt();
+
+
+    class Monster {
+        constructor() {
+            this.x = 0;
+            this.y = 0;
+            this.imgMonster = new Image();
+            this.imgMonster.src = "./../bilder/Profile.jpg";
+        }
+
+        reset() {}
+
+
+        ritaMonster() {
+            ctx.beginPath();
+            ctx.drawImage(this.imgMonster, this.x * 60 + 5, this.y * 60 + 5, 55, 55);
+            ctx.closePath();
+        }
+        uppdateraMonster() {
+
+            var gamlaMonsterY = monster.y;
+            var gamlaMonsterX = monster.x;
+
+            monster.x += Math.ceil(Math.random() * 3 - 2);
+            monster.y += Math.ceil(Math.random() * 3 - 2);
+
+            if (map[monster.y][monster.x] == 1) {
+                monster.x = gamlaMonsterX;
+                monster.y = gamlaMonsterY;
+            }
+        }
+    }
+
+    var monster1 = new Monster();
+    var monster2 = new Monster();
 
     /* Kloss  */
     function ritaKloss(x, y) {
@@ -61,45 +119,35 @@ function start() {
         ctx.closePath();
     }
 
-    function ritaSpelare(x, y) {
+    function ritaSpelare() {
         ctx.beginPath();
-        ctx.drawImage(imgSpelare, x * 60 + 5, y * 60 + 5, 55, 55);
+        ctx.drawImage(imgSpelare, spelare.x * 60 + 5, spelare.y * 60 + 5, 55, 55);
         ctx.closePath();
     }
 
-    function ritaMonster(x, y) {
-        ctx.beginPath();
-        ctx.drawImage(imgMonster, x * 60 + 5, y * 60 + 5, 55, 55);
-        ctx.closePath();
+    function highscore() {
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "#red";
+        ctx.fillText("Poäng: " + points, 600, 50);
     }
 
-    function uppdateraMonster() {
-        monster.x += Math.ceil(Math.random() * 5 - 2);
-        monster.y += Math.ceil(Math.random() * 5 + 2);
+    function reset() {
+        spelare.y = 1;
+        spelare.x = 1;
 
-        if (monster.x < 0) {
-            monster.x = 12;
-        }
-        if (monster.x > 12) {
-            monster.x = 0;
-        }
-        if (monster.y < 0) {
-            monster.y = 12;
-        }
-        if (monster.y > 12) {
-            monster.y = 0;
-        }
+        monster.y = 2;
+        monster.x = 4;
+
+        mynt1.reset();
+        mynt2.reset();
     }
-
-    setInterval(uppdateraMonster, 600)
-
 
     function ritaLabyrint() {
         /* Rita ut mapen i arrayen */
         /* Höjden */
         for (var j = 0; j < 10; j++) {
             /* Bredden */
-            for (var i = 0; i < 12; i++) {
+            for (var i = 0; i < 13; i++) {
                 if (map[j][i] == 1) {
                     ritaKloss(i * 60, j * 60);
                 }
@@ -107,14 +155,11 @@ function start() {
         }
     }
 
-    function dead() {
-        if (monster.x == spelare.x && monster.y == spelare.y) {
-            alert ("dead");
-        }
-    }
+    setInterval(uppdateraMonster, 100);
 
 
     /* Lyssna på piltangenterna */
+
     document.addEventListener("keydown", uppdateraSpelare);
 
     function uppdateraSpelare(e) {
@@ -135,12 +180,19 @@ function start() {
             spelare.y += 1;
         }
 
-
         if (map[spelare.y][spelare.x] == 1) {
             spelare.x = gamlaX;
             spelare.y = gamlaY;
         }
 
+    }
+
+
+    function dead() {
+        if (monster.x == spelare.x && monster.y == spelare.y) {
+            alert("Dead!");
+            reset();
+        }
     }
 
 
@@ -150,6 +202,15 @@ function start() {
         ctx.clearRect(0, 0, 800, 600);
         ritaLabyrint();
         dead();
+
+        mynt1.uppdateraMynt();
+        mynt2.uppdateraMynt();
+        mynt1.getPoint();
+        mynt2.getPoint();
+        
+        monster1();
+        monster2();
+        highscore();
         ritaSpelare(spelare.x, spelare.y);
         ritaMonster(monster.x, monster.y);
         requestAnimationFrame(gameLoop);
